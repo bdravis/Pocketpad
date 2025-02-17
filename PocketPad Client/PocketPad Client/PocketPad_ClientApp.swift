44be2d4f134da29984fc26e93c526a79ecc43a57
leminlimez <59540996+leminlimez@users.noreply.github.com>
2025-02-17 14:39:55 -0500

    create the xcode project

---
+++ PocketPad Client/PocketPad Client/PocketPad_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketPad_ClientApp.swift
+//  PocketPad Client
+//
+//  Created by lemin on 2/17/25.
+//
+
+import SwiftUI
+
+@main
+struct PocketPad_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PocketPad Client/PocketPad ClientTests/PocketPad_ClientTests.swift
@@ -0,0 +1,17 @@
+//
+//  PocketPad_ClientTests.swift
+//  PocketPad ClientTests
+//
+//  Created by lemin on 2/17/25.
+//
+
+import Testing
+@testable import PocketPad_Client
+
+struct PocketPad_ClientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
