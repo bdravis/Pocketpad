821a124569fc7d305d4cc6f44e6cc63e636c3aa2
leminlimez <59540996+leminlimez@users.noreply.github.com>
2025-02-18 07:50:21 -0500

    change the main configuration to a protocol

--- PocketPad Client/PocketPad Client/Objects/ButtonConfig.swift
+++ PocketPad Client/PocketPad Client/Objects/ButtonConfig.swift
@@ -14,35 +14,42 @@ enum ButtonType: Codable {
     case dpad
 }
 
-// Configuration of the buttons for the layout
-struct ButtonConfig: Codable {
-    var position: CGPoint // position of btn on screen
-    var scale: CGFloat // % scale of the button (1.0 = 100% scale)
+// Protocol for configuration of the buttons for the layout
+protocol ButtonConfig: Codable {
+    var position: CGPoint { get set } // position of btn on screen
+    var scale: CGFloat { get set } // % scale of the button (1.0 = 100% scale)
     
-    var type: ButtonType // what type of button it is
-    var data: ButtonData
-}
-
-// Enum to be able to store multiple controller types in the same ButtonConfig struct
-enum ButtonData: Codable {
-    case regular(RegularButtonData)
-    case joystick(JoystickData)
-    case dpad(DPadData)
+    var type: ButtonType { get set } // what type of button it is
 }
 
 // MARK: Button Data Objects
-struct RegularButtonData: Codable {
+struct RegularButtonData: ButtonConfig {
+    // Protocol Properties
+    var position: CGPoint
+    var scale: CGFloat
+    var type: ButtonType
+    
     // TODO: Decide whether input will be a string or an int correlating to button id
     var input: String // the button it is bound to
     var turbo: Bool // whether or not it is a turbo tap
 }
 
-struct JoystickData: Codable {
+struct JoystickData: ButtonConfig {
+    // Protocol Properties
+    var position: CGPoint
+    var scale: CGFloat
+    var type: ButtonType
+    
     var input: String // which joystick button it will correlate to
     var sensitivity: Double // the sensitivity of the controller
     var deadzone: Double // how far it needs to move before it starts accepting inputs
 }
 
-struct DPadData: Codable {
+struct DPadData: ButtonConfig {
+    // Protocol Properties
+    var position: CGPoint
+    var scale: CGFloat
+    var type: ButtonType
+    
     var inputs: [String] // what the buttons of the dpad are
 }
