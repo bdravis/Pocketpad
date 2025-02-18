f14356b838a31725ddbaaac99440578c86d24a7a
leminlimez <59540996+leminlimez@users.noreply.github.com>
2025-02-18 10:41:05 -0500

    conform btn to identifiable + scaling func for ui

--- PocketPad Client/PocketPad Client/Objects/ButtonConfig.swift
+++ PocketPad Client/PocketPad Client/Objects/ButtonConfig.swift
@@ -7,6 +7,9 @@
 
 import UIKit
 
+// MARK: Configuration Constants
+let DEFAULT_BUTTON_SIZE: CGFloat = 50.0
+
 // Enum for the type of button it is
 enum ButtonType: Codable {
     case regular
@@ -16,10 +19,51 @@ enum ButtonType: Codable {
 
 // Protocol for configuration of the buttons for the layout
 protocol ButtonConfig: Codable {
+    var id: UUID { get }
     var position: CGPoint { get set } // position of btn on screen
     var scale: CGFloat { get set } // % scale of the button (1.0 = 100% scale)
     
     var type: ButtonType { get set } // what type of button it is
+}
+
+extension ButtonConfig {
+    var id: UUID {      // conform to identifiable
+        return UUID()
+    }
+    
+    mutating func getScaledPosition(bounds: CGRect? = nil) -> CGPoint {
+        // Position the button so that it is not clipped by the edge of the screen
+        let scaledSize = (DEFAULT_BUTTON_SIZE * self.scale) / 2
+        var fixedPos = self.position
+        
+        if let frame = bounds {
+            // prevent from going off the right/bottom of the screen if bounds are supplied
+            if fixedPos.x + scaledSize > frame.maxX {
+                fixedPos.x = frame.maxX - scaledSize
+            }
+            if fixedPos.y + scaledSize > frame.maxY {
+                fixedPos.y = frame.maxY - scaledSize
+            }
+            
+            // prevent from going off the left/top of the screen if bounds are supplied
+            if fixedPos.x - scaledSize < frame.minX {
+                fixedPos.x = frame.minX + scaledSize
+            }
+            if fixedPos.y - scaledSize < frame.minY {
+                fixedPos.y = frame.minY + scaledSize
+            }
+        } else {
+            // prevent from going off the left/top of the screen without provided bounds (position 0)
+            if fixedPos.x - scaledSize < 0.0 {
+                fixedPos.x = scaledSize
+            }
+            if fixedPos.y - scaledSize < 0.0 {
+                fixedPos.y = scaledSize
+            }
+        }
+        
+        return fixedPos
+    }
 }
 
 // MARK: Specific Button Config Objects
