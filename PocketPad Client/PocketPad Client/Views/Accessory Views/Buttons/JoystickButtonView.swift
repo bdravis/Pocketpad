1f2944095639eaef9165afa82101d00aaef2d382
leminlimez <59540996+leminlimez@users.noreply.github.com>
2025-02-19 15:56:50 -0500

    create basic joystick design - not functional

--- PocketPad Client/PocketPad Client/Views/Accessory Views/Buttons/JoystickButtonView.swift
+++ PocketPad Client/PocketPad Client/Views/Accessory Views/Buttons/JoystickButtonView.swift
@@ -7,14 +7,22 @@
 
 import SwiftUI
 
+let STICK_SIZE = DEFAULT_BUTTON_SIZE / 3
+
 struct JoystickButtonView: View {
     var config: JoystickConfig
     
     var body: some View {
         ZStack {
-            Rectangle()
-                .foregroundStyle(.green)
-            Text("Joystick")
+            // Background
+            Circle()
+                .strokeBorder(.black, style: StrokeStyle(lineWidth: 3))
+                .foregroundStyle(Color(uiColor: .secondarySystemFill))
+            
+            // Stick itself
+            Circle()
+                .frame(width: STICK_SIZE, height: STICK_SIZE)
+                .foregroundStyle(Color(uiColor: .secondaryLabel))
         }
     }
 }
