7fb01efce263bb4969460fd78587c4d7b22247c6
leminlimez <59540996+leminlimez@users.noreply.github.com>
2025-03-06 16:04:41 -0500

    Revert "Attempt to add button holding"
    This reverts commit 320874f1dd9fc5555cf48672c73ee97ee2b0d73c.

--- PocketPad Client/PocketPad Client/Views/Accessory Views/Buttons/RegularButtonView.swift
+++ PocketPad Client/PocketPad Client/Views/Accessory Views/Buttons/RegularButtonView.swift
@@ -11,73 +11,32 @@ struct RegularButtonView: View {
     @StateObject private var bluetoothManager = BluetoothManager.shared
     var config: RegularButtonConfig
     
-    @State private var isHolding: Bool = false
-    
-    // gesture logic that contains the actions for when the button is held
-    var buttonHold: some Gesture {
-        DragGesture(minimumDistance: 0)
-            .onChanged { _ in
-                if !isHolding {
-                    // Initial action
-                    
-                    isHolding = true
-                }
-                
-                // Action while held
-                if let service = bluetoothManager.selectedService {
-                    let ui8_playerId: UInt8 = 0 // Assuming one player
-                    let ui8_inputId : UInt8 = config.inputId
-                    let ui8_buttonType : UInt8 = config.type.rawValue
-                    
-                    // send data continuously
-                    let data = Data([ui8_playerId, ui8_inputId, ui8_buttonType])
-                    bluetoothManager.sendInput(data)
-                }
-                
-            }
-            .onEnded { _ in
-                // Action when released
-                
-                isHolding = false
-            }
-    }
-    
     var body: some View {
         Button(action: {
-            // The action for holding is located in the label closure in the gesture modifier
-        }) {
-            Group {
-                if let icon = config.style.icon {
-                    switch config.style.iconType {
-                    case .Text:
-                        Text(icon)
-                    case .SFSymbol:
-                        ZStack {
-                            Image(systemName: icon)
-                                .resizable()
-                                .scaledToFit() // make sure it does not stretch
-                        }
-                    }
-                } else {
-                    Text("") // empty textbox
-                }
+            if let service = bluetoothManager.selectedService {
+                let ui8_playerId: UInt8 = 0 // Assuming one player
+                let ui8_inputId : UInt8 = config.inputId
+                let ui8_buttonType : UInt8 = config.type.rawValue
+                
+                let data = Data([ui8_playerId, ui8_inputId, ui8_buttonType])
+                bluetoothManager.sendInput(data)
+                
             }
-            .gesture(buttonHold) // apply gesture logic
-            
-//            if let icon = config.style.icon {
-//                switch config.style.iconType {
-//                case .Text:
-//                    Text(icon)
-//                case .SFSymbol:
-//                    ZStack {
-//                        Image(systemName: icon)
-//                            .resizable()
-//                            .scaledToFit() // make sure it does not stretch
-//                    }
-//                }
-//            } else {
-//                Text("") // empty textbox
-//            }
+        }) {
+            if let icon = config.style.icon {
+                switch config.style.iconType {
+                case .Text:
+                    Text(icon)
+                case .SFSymbol:
+                    ZStack {
+                        Image(systemName: icon)
+                            .resizable()
+                            .scaledToFit() // make sure it does not stretch
+                    }
+                }
+            } else {
+                Text("") // empty textbox
+            }
         }
         .applyButtonStyle(shape: config.style.shape)
     }
