81a2cb3be460938f7da7e78dad571e2184c2ae79
leminlimez <59540996+leminlimez@users.noreply.github.com>
2025-03-07 14:22:39 -0500

    saving, loading, and deleting layouts

--- PocketPad Client/PocketPad Client/Views/SettingsMenuView.swift
+++ PocketPad Client/PocketPad Client/Views/SettingsMenuView.swift
@@ -23,6 +23,8 @@ struct SettingsMenuView: View {
     @AppStorage("controllerName") var controllerName: String = "Controller"
     
     @State private var showDPadStyle: Bool = false
+    @State private var saveAsMalformed: Bool = false
+    @State private var availableControllers: [String] = [] // TODO: Change this to a VM later
     
     // MARK: - Body
     var body: some View {
@@ -98,8 +100,11 @@ struct SettingsMenuView: View {
                     .foregroundColor(.primary)
                 Spacer()
                 Picker("Controller Type", selection: $selectedController) {
-                    ForEach(ControllerType.allCases, id: \.self) { type in
-                        Label(type.stringValue, image: type.stringValue).tag(type.stringValue)
+//                    ForEach(ControllerType.allCases, id: \.self) { type in
+//                        Label(type.stringValue, image: type.stringValue).tag(type.stringValue)
+//                    }
+                    ForEach(availableControllers, id: \.self) { layout in
+                        Label(layout, image: layout).tag(layout)
                     }
                 }
                 .pickerStyle(.menu)
@@ -110,12 +115,13 @@ struct SettingsMenuView: View {
                         try LayoutManager.shared.setCurrentLayout(to: selectedController)
                         showDPadStyle = LayoutManager.shared.hasDPad
                     } catch {
-                        print(error.localizedDescription)
-                        // TODO: alert user of error
+                        UIApplication.shared.alert(title: "Failed to load layout", body: error.localizedDescription)
+                        selectedController = ControllerType.Xbox.stringValue
                     }
                 }
                 .onAppear {
                     showDPadStyle = LayoutManager.shared.hasDPad
+                    availableControllers = LayoutManager.shared.availableLayouts
                 }
             }
             .padding(.horizontal, 16)
@@ -137,31 +143,112 @@ struct SettingsMenuView: View {
             }
             
             // Controller Color Section
-            VStack(alignment: .leading, spacing: 10) {
-                HStack {
-                    Text("Controller Color")
-                        .foregroundColor(.primary)
-                    
-                    Spacer()
-                    
-                    ColorPicker("", selection: $controllerColor, supportsOpacity: false)
-                        .labelsHidden()
-                        .padding(.trailing, 16)
-                        .accessibilityIdentifier("ControllerColorPicker")
-
-                }
-                .padding(.horizontal, 16)
-            }
             HStack {
-                            Text("Controller Name")
-                                .foregroundColor(.primary)
-                            Spacer()
-                            TextField("Enter controller name", text: $controllerName)
-                                .textFieldStyle(RoundedBorderTextFieldStyle())
-                                .accessibilityIdentifier("NameField")
-                        }
-                        .padding(.horizontal, 16)
+                Text("Controller Color")
+                    .foregroundColor(.primary)
+                
+                Spacer()
+                
+                ColorPicker("", selection: $controllerColor, supportsOpacity: false)
+                    .labelsHidden()
+                    .padding(.trailing, 16)
+                    .accessibilityIdentifier("ControllerColorPicker")
+
+            }
+            .padding(.horizontal, 16)
+            HStack {
+                Text("Controller Name")
+                    .foregroundColor(.primary)
+                Spacer()
+                TextField("Enter controller name", text: $controllerName)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .accessibilityIdentifier("NameField")
+            }
+            .padding(.horizontal, 16)
+            
+            // MARK: Saving layouts (temporary)
+            Section {
+                // Toggle to save layout as malformed
+                Toggle("Save as malformed file", isOn: $saveAsMalformed)
+                
+                // Picker to choose a layout to save
+                HStack {
+                    Text("Save layout")
+                    Spacer()
+                    Button(action: {
+                        showSaveLayoutPopup()
+                    }) {
+                        Text("Choose Template")
+                    }
+                }
+                
+                // Remove files for layout
+                Button(action: {
+                    do {
+                        try LayoutManager.shared.setCurrentLayout(to: ControllerType.Xbox.stringValue)
+                        try LayoutManager.shared.deleteAllLayouts()
+                        try LayoutManager.shared.loadLayouts(includeControllerTypes: true)
+                        selectedController = ControllerType.Xbox.stringValue
+                        availableControllers = LayoutManager.shared.availableLayouts
+                    } catch {
+                        UIApplication.shared.alert(body: error.localizedDescription)
+                    }
+                }) {
+                    Text("Remove all file layouts")
+                }
+                .foregroundStyle(.red)
+            } header: {
+                Text("Layouts (testing)")
+                    .font(.footnote)
+                    .foregroundStyle(Color(uiColor: .secondaryLabel))
+            }
+            .padding(.horizontal, 16)
         }
+    }
+    
+    func saveLayoutFile(for controller: ControllerType) {
+        var layout = DefaultLayouts.getLayout(for: controller)
+        layout.name = "\(controller.stringValue) Saved"
+        do {
+            if saveAsMalformed {
+                try LayoutManager.shared.saveMalformedLayout(layout)
+            } else {
+                try LayoutManager.shared.saveLayout(layout)
+            }
+            try LayoutManager.shared.loadLayouts(includeControllerTypes: true)
+            availableControllers = LayoutManager.shared.availableLayouts
+            UIApplication.shared.alert(title: "Layout Successfully Saved", body: "It can be found in the \"Controller Type\" menu.")
+        } catch {
+            UIApplication.shared.alert(body: "Failed to save the layout:\n\(error.localizedDescription)")
+        }
+    }
+    
+    func showSaveLayoutPopup() {
+        // TODO: refactor this later to an extension
+        let alert = UIAlertController(title: "Choose a layout to save", message: "It will save it as a \(!saveAsMalformed ? "non-" : "")malformed file", preferredStyle: .actionSheet)
+        
+        // add the actions
+        let xboxAction = UIAlertAction(title: "Xbox", style: .default) { (action) in
+            saveLayoutFile(for: .Xbox)
+        }
+        alert.addAction(xboxAction)
+        let wiiAction = UIAlertAction(title: "Wii", style: .default) { (action) in
+            saveLayoutFile(for: .Wii)
+        }
+        alert.addAction(wiiAction)
+        
+        let cancelAction = UIAlertAction(title: NSLocalizedString("Cancel", comment: ""), style: .cancel) { (action) in
+            // cancels the action
+        }
+        alert.addAction(cancelAction)
+        
+        let view: UIView = UIApplication.shared.windows.first!.rootViewController!.view
+        // present popover for iPads
+        alert.popoverPresentationController?.sourceView = view // prevents crashing on iPads
+        alert.popoverPresentationController?.sourceRect = CGRect(x: view.bounds.midX, y: view.bounds.maxY, width: 0, height: 0) // show up at center bottom on iPads
+        
+        // present the alert
+        UIApplication.shared.windows.first?.rootViewController?.present(alert, animated: true)
     }
 }
 
