001e43ece82882bcab92575d2a13af3baed1cbea
leminlimez <59540996+leminlimez@users.noreply.github.com>
2025-03-30 17:58:17 -0400

    add text input to mitigate slider issue

--- PocketPad Client/PocketPad Client/Views/Accessory Views/ButtonEditor/EditButtonView.swift
+++ PocketPad Client/PocketPad Client/Views/Accessory Views/ButtonEditor/EditButtonView.swift
@@ -12,79 +12,100 @@ struct EditButtonView: View {
     @ObservedObject private var layoutManager = LayoutManager.shared
     // TODO: Figure out why it does not continuously update
     
+    private var numberFormatter: NumberFormatter {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        return nf
+    }
+    
     var body: some View {
         List {
             Section {
-                VStack {
-                    // MARK: X Scale
-                    HStack {
-                        Text("X Scale")
-                        Spacer()
-                        Text("\(layoutManager.currentController.buttons[buttonId].position.scaledPos.x)")
-                            .foregroundStyle(.blue)
-                    }
-                    Slider(value: $layoutManager.currentController.buttons[buttonId].position.scaledPos.x, in: 0...1, step: 0.01) {
-                        Text("X")
-                    } minimumValueLabel: {
-                        Text("0.0")
-                    } maximumValueLabel: {
-                        Text("1.0")
-                    }
-                }
+                // MARK: X Scale
+                EditorSlider(title: "X Scale", value: $layoutManager.currentController.buttons[buttonId].position.scaledPos.x, min: 0.0, max: 1.0, step: 0.01, inputWidth: 40, keyboardType: .decimalPad, formatter: numberFormatter)
                 // MARK: Y Scale
-                VStack {
-                    HStack {
-                        Text("Y Scale")
-                        Spacer()
-                        Text("\(layoutManager.currentController.buttons[buttonId].position.scaledPos.y)")
-                            .foregroundStyle(.blue)
-                    }
-                    Slider(value: $layoutManager.currentController.buttons[buttonId].position.scaledPos.y, in: 0...1, step: 0.01) {
-                        Text("Y")
-                    } minimumValueLabel: {
-                        Text("0.0")
-                    } maximumValueLabel: {
-                        Text("1.0")
-                    }
-                }
+                EditorSlider(title: "Y Scale", value: $layoutManager.currentController.buttons[buttonId].position.scaledPos.y, min: 0.0, max: 1.0, step: 0.01, inputWidth: 40, keyboardType: .decimalPad, formatter: numberFormatter)
             } header: {
                 Text("Position")
             }
             
             Section {
-                VStack {
-                    // MARK: Scale
-                    HStack {
-                        Text("Scale")
-                        Spacer()
-                        Text("\(layoutManager.currentController.buttons[buttonId].scale * 100)%")
-                            .foregroundStyle(.blue)
-                    }
-                    Slider(value: $layoutManager.currentController.buttons[buttonId].scale, in: 0.25...4.0, step: 0.05) {
-                        Text("Scale")
-                    } minimumValueLabel: {
-                        Text("25%")
-                    } maximumValueLabel: {
-                        Text("400%")
-                    }
+                // MARK: Scale
+                EditorSlider(title: "Scale", value: $layoutManager.currentController.buttons[buttonId].scale, min: 0.25, max: 4.0, step: 0.05, inputWidth: 40, keyboardType: .decimalPad, formatter: numberFormatter)
+                // MARK: Rotation
+                EditorSlider(title: "Rotation", value: $layoutManager.currentController.buttons[buttonId].rotation, units: "º", min: 0.0, max: 360.0, step: 1.0, inputWidth: 40, keyboardType: .numberPad, formatter: numberFormatter)
+            }
+        }
+    }
+}
+
+struct EditorSlider<V>: View where V : BinaryFloatingPoint, V.Stride : BinaryFloatingPoint {
+    var title: String
+    @Binding var value: V
+    var units: String = ""
+    var min: V
+    var minLabel: String? = nil
+    var max: V
+    var maxLabel: String? = nil
+    var step: V
+    
+    var inputWidth: CGFloat
+    var keyboardType: UIKeyboardType
+    var formatter: Formatter
+    
+    @State var enterAlert: Bool = false
+    @State var enteringValue: V = 0.0
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(title)
+                Spacer()
+                Button(action: {
+                    enteringValue = value
+                    enterAlert.toggle()
+                }) {
+                    Text("\(value)\(units)")
                 }
-                VStack {
-                    // MARK: Rotation
-                    HStack {
-                        Text("Rotation")
-                        Spacer()
-                        Text("\(layoutManager.currentController.buttons[buttonId].rotation)º")
-                            .foregroundStyle(.blue)
+                .alert("Enter Value", isPresented: $enterAlert, actions: {
+                    TextField(title, value: $enteringValue, formatter: formatter)
+                        .keyboardType(keyboardType)
+                    Button("Cancel") {
+                        enterAlert = false
                     }
-                    Slider(value: $layoutManager.currentController.buttons[buttonId].rotation, in: 0.0...360.0, step: 1.0) {
-                        Text("Rotation")
-                    } minimumValueLabel: {
-                        Text("0º")
-                    } maximumValueLabel: {
-                        Text("360º")
+                    Button("Done") {
+                        verifyInputRange(v: &enteringValue)
+                        value = enteringValue
+                        enterAlert = false
                     }
+                }) {
+                    Text("Enter a value for \(title).")
                 }
             }
+            Slider(value: $value, in: min...max, step: V.Stride(step)) {
+                Text(title)
+            } minimumValueLabel: {
+                if let minLabel = minLabel {
+                    Text(minLabel)
+                } else {
+                    Text("\(min)\(units)")
+                }
+            } maximumValueLabel: {
+                if let maxLabel = maxLabel {
+                    Text(maxLabel)
+                } else {
+                    Text("\(max)\(units)")
+                }
+            }
+        }
+    }
+    
+    func verifyInputRange(v: inout V) {
+        if v < min {
+            v = min
+        }
+        if v > max {
+            v = max
         }
     }
 }
