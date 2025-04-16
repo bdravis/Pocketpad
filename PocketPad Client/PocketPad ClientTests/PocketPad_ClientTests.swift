00eecbc24bc8fa32df2fb02bc55cfaafb75dcca0
leminlimez <59540996+leminlimez@users.noreply.github.com>
2025-04-16 19:50:38 -0400

    make client tests more predictable

--- PocketPad Client/PocketPad ClientTests/PocketPad_ClientTests.swift
+++ PocketPad Client/PocketPad ClientTests/PocketPad_ClientTests.swift
@@ -26,7 +26,7 @@ struct PocketPad_ClientTests {
         // checks export/import of all of the controller types
         for controller in ControllerType.allCases {
             let initialLayout = DefaultLayouts.getLayout(for: controller)
-            try LayoutManager.shared.saveLayout(DefaultLayouts.getLayout(for: controller))
+            try LayoutManager.shared.saveLayout(initialLayout)
             let loadedLayout = try LayoutManager.shared.loadLayout(for: controller.stringValue)
             #expect(loadedLayout == initialLayout)
         }
@@ -34,7 +34,7 @@ struct PocketPad_ClientTests {
         try LayoutManager.shared.loadLayouts() // load the controllers to make sure the files exist
         #expect(LayoutManager.shared.availableLayouts.count >= 7)
         for controller in ControllerType.allCases {
-            #expect(LayoutManager.shared.availableLayouts.contains(controller.stringValue))
+            #expect(LayoutManager.shared.availableLayouts.contains(DefaultLayouts.getLayout(for: controller).name))
         }
         try LayoutManager.shared.deleteAllLayouts()
     }
