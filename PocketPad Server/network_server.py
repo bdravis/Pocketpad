7c9f90765ef82fc04be12907c24b744449e0cc72
bdravis <bdravis@purdue.edu>
2025-05-02 15:06:11 -0400

    fixed regex

--- PocketPad Server/network_server.py
+++ PocketPad Server/network_server.py
@@ -76,8 +76,8 @@ def is_allowed_path(path: str) -> bool:
 def extract_game_name_from_path(full_path: str) -> str:
     core, *_ = full_path.rsplit(" ", 1)
     file_name = os.path.basename(core)
-    name, _ = os.path.splitext*(file_name)
-    cleaned = re.sub(r'\s*[\(\[].*?[\)\]])]\s*$', '', name).strip()
+    name, _ = os.path.splitext(file_name)
+    cleaned = re.sub(r'\s*[\(\[].*?[\)\]]\s*$', '', name).strip()
     return cleaned
 
 # async def prefixed_send(writer: asyncio.StreamWriter, payload: bytes) -> None:
